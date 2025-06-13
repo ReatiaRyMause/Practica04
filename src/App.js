1575896a701fccd1326d020adf682062f2d7d0e1
ReatiaRyMause <victormanuelarredondo054@gmail>
2025-06-12 18:44:46 -0600

    cambio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-                    Victor Arredondo <code>src/App.js</code> and save to reload.
+                    Victor Manuel Arredondo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
